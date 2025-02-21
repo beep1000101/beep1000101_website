6c8dc305800406b14b958259506e5f3ed13eeda0
beep1000101 <matiponiatowski@o2.pl>
2025-02-21 22:10:43 +0100

    hello world

---
+++ hello.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            First webpage of mine
+        </title>
+    </head>
+    <body>
+        <?php echo "Hello World!" ?>
+    </body>
+</html>
