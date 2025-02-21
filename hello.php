7d76f480e7eed5c8c3d252c74274386dfe088b48
beep1000101 <matiponiatowski@o2.pl>
2025-02-22 00:28:00 +0100

    update hello

--- hello.php
+++ hello.php
@@ -8,4 +8,10 @@
     <body>
         <?php echo "Hello World!" ?>
     </body>
+    <p class="editor note">
+        My cat is super grumpy kitty cat.
+    </p>
+    <?php
+        echo $_SERVER['HTTP_USER_AGENT'];
+    ?>
 </html>
